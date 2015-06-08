bf6ce12e9757e4f251a457022abafbaace68c66e
Phuong Le <tungphuongld@gmail.com>
2015-06-08 18:24:52 +0700

    no message

--- vnext/src/helloVnext/Startup.cs
+++ vnext/src/helloVnext/Startup.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Diagnostics;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
 using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
 using helloVnext.Models;
 
 namespace helloVnext
@@ -50,20 +42,20 @@ namespace helloVnext
             // Configure the HTTP request pipeline.
 
             // Add the console logger.
-            loggerfactory.AddConsole();
+            //loggerfactory.AddConsole();
 
-            // Add the following to the request pipeline only in development environment.
-            if (env.IsEnvironment("Development"))
-            {
-                app.UseBrowserLink();
-                app.UseErrorPage(ErrorPageOptions.ShowAll);
-            }
-            else
-            {
-                // Add Error handling middleware which catches all application specific errors and
-                // send the request to the following path or controller action.
-                app.UseErrorHandler("/Home/Error");
-            }
+            //// Add the following to the request pipeline only in development environment.
+            //if (env.IsEnvironment("Development"))
+            //{
+            //    app.UseBrowserLink();
+            //    app.UseErrorPage(ErrorPageOptions.ShowAll);
+            //}
+            //else
+            //{
+            //    // Add Error handling middleware which catches all application specific errors and
+            //    // send the request to the following path or controller action.
+            //    app.UseErrorHandler("/Home/Error");
+            //}
 
             // Add static files to the request pipeline.
             app.UseStaticFiles();
