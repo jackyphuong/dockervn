8b0375422589e2ce775a576ae6ad02b93594d4bd
Phuong Le <tungphuongld@gmail.com>
2015-06-08 16:53:09 +0700

    init proj

---
+++ vnext/helloVnext/src/helloVnext/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace helloVnext.Controllers
+{
+    public class HomeController : Controller
+    {
+        [FromServices]
+        public HelloVnextContext DbContext { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        public IActionResult Environment()
+        {          
+            ViewBag.Message = "Your application description page.";
+            var environments = System.Environment.GetEnvironmentVariables();
+            return View(environments);
+        }
+
+        public async Task<IActionResult> TopSellingAlbums()
+        {
+            var albums = await DbContext.Albums
+                .OrderByDescending(x=>x.Price)
+                .Take(10)
+                .ToListAsync();
+            return View(albums);
+        }
+    }
+}
---
+++ vnext/helloVnext/src/helloVnext/HelloVnextContext.cs
@@ -0,0 +1,21 @@
+﻿using helloVnext.Models;
+using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloVnext
+{
+    public class HelloVnextContext:DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Album>().Key(a => a.AlbumId);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
